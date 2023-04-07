d8bc9c1bcbe623a2cb5d7bd5d552c3556e1c15ae
Zese07 <128886474+Zese07@users.noreply.github.com>
2023-04-07 21:48:25 +0800

    Added strength checker.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import PySimpleGUI as sg
 import random
+import re
 
 letters = [chr(letter) for letter in range(ord('a'), ord('z')+1)] +\
           [chr(letter) for letter in range(ord('A'), ord('Z')+1)]
@@ -11,7 +12,7 @@ numbers = list(range(10))
 generate = False
 password = ""
 
-layout = [[sg.Text('Input the length of the password:'),
+layout = [[sg.Text('Input the length of the password:', font=('', 10, 'bold')),
            sg.Input(key='LENGTH', size=(10, 1), justification='center')],
     [sg.Radio('Letters', 'TOP', default=True, key='LETTERS'),
      sg.Radio('Numbers', 'TOP', key='NUMBERS'),
@@ -21,12 +22,73 @@ layout = [[sg.Text('Input the length of the password:'),
      sg.Radio('Only Upper', 'BOT', key='ONLYUPPER'),
      sg.Radio('Only Lower', 'BOT', key='ONLYLOWER')],
     [sg.Button('Generate', bind_return_key=True)],
-    [sg.Text('Password:'),
-     sg.Input(key="PASSWORD", size=(30, 1)),
-     sg.Button('Copy', size=(5, 1), bind_return_key=True)]]
+    [sg.Text('Strength:', font=('', 10, 'bold')), sg.Text('NONE', key='STRENGTH', font=('', 10, 'bold'))],
+    [sg.Text('Password:', font=('', 10, 'bold')),
+     sg.Input(key="PASSWORD", size=(30, 1), justification='center'),
+     sg.Button('Copy', size=(5, 1), bind_return_key=True),
+     sg.Button('Check', size=(5, 1), bind_return_key=True)]]
 
-window = sg.Window('Simple Password Generator', layout, size=(400, 160), element_justification='c', finalize=True)
+window = sg.Window('Simple Password Generator', layout, size=(450, 200), element_justification='c', finalize=True)
 window.set_icon('icon.ico')
+
+
+def check_password(password):
+    length_count = len(password) * 0.25
+    letters_count = len(re.findall(r'[a-z]', password)) * 1.25 + len(re.findall(r'[A-Z]', password)) * 1.5
+
+    symbols_count = 0
+    for symbols in password:
+        if 33 <= ord(symbols) <= 47 or 58 <= ord(symbols) <= 64 or 91 <= ord(symbols) <= 96 or 123 <= ord(symbols) <= 126:
+            symbols_count += 1.5
+
+    numbers_count = len(re.findall(r'\d', password)) * 1.15
+    score = length_count + letters_count + symbols_count + numbers_count
+
+    unique = 3
+    if not re.search(r'[a-zA-Z]', password):
+        unique -= 1
+    if symbols_count == 0:
+        unique -= 1
+    if not re.search(r'\d', password):
+        unique -= 1
+    else:
+        pass
+
+    if unique == 3:
+        if len(password) <= 10:
+            score += 2.5
+        elif 10 < len(password) <= 20:
+            pass
+        else:
+            score -= 2.5
+    elif unique == 2:
+        if len(password) <= 10:
+            score -= 1
+        elif 10 < len(password) <= 20:
+            score -= 3.5
+        else:
+            score -= 6
+    elif unique == 1:
+        if len(password) <= 10:
+            score -= 4.5
+        elif 10 < len(password) <= 20:
+            score -= 7
+        else:
+            score -= 9.5
+
+    if score <= 7:
+        window['STRENGTH'].update('Very Weak', text_color='red')
+    elif 7 < score <= 11:
+        window['STRENGTH'].update('Weak', text_color='orange')
+    elif 11 < score <= 16:
+        window['STRENGTH'].update('Good', text_color='yellow')
+    elif 16 < score <= 22:
+        window['STRENGTH'].update('Great', text_color='yellowgreen')
+    elif 22 < score <= 29:
+        window['STRENGTH'].update('Strong', text_color='green')
+    else:
+        window['STRENGTH'].update('Very Strong', text_color='lime')
+
 
 while True:
     event, values = window.read()
@@ -92,6 +154,12 @@ while True:
         else:
             sg.clipboard_set(values['PASSWORD'])
             sg.popup('Successfully copied to clipboard.', title='/', icon='icon.ico')
+
+    elif event == 'Check':
+        if values['PASSWORD'] == '':
+            sg.popup('There is no password to check.', title='X', icon='icon.ico')
+        else:
+            check_password(values['PASSWORD'])
 
 
 window.close()
